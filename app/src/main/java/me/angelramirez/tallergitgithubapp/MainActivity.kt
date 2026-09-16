c064f00d9c56e9fe8ee11d81579e9241a023cd3f
Angel Ramirez <Aramirez181@unab.edu.co>
2026-09-16 15:25:17 -0500

    Avance: foto personal

--- app/src/main/java/me/angelramirez/tallergitgithubapp/MainActivity.kt
+++ app/src/main/java/me/angelramirez/tallergitgithubapp/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border // Faltaba esta importación
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip // Faltaba esta importación
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.Cyan
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import me.angelramirez.tallergitgithubapp.ui.theme.TallerGitGithubAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +33,36 @@ class MainActivity : ComponentActivity() {
         setContent {
             TallerGitGithubAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        MifotodePerfil()
+                    }
                 }
             }
         }
     }
 }
 
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TallerGitGithubAppTheme {
-        Greeting("Android")
+fun MifotodePerfil() {
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize()
+            .padding(top = 40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.yopro3),
+            contentDescription = "Foto pro de mi",
+            modifier = Modifier
+                .size(150.dp)
+                .clip(CircleShape)
+                .border(4.dp, Color.Blue, CircleShape)
+        )
     }
 }
